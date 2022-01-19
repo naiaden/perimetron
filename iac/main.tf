0cdc3d2141291bdffadb2e38746dc90d3f51db0d
louis <1160507+naiaden@users.noreply.github.com>
2022-01-19 22:50:26 +0100

    Add terraform deployment through pipeline (#1)
    feat(pipeline): add initial github workflow
    feat(terraform): add initial infrastructure for table storage
    
    Co-authored-by:  <louis@arethusa.localdomain>

--- iac/main.tf
+++ iac/main.tf
@@ -1,17 +1,42 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "naiadentfstate"
+    container_name       = "tfstate"
+    key                  = "perimetron.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.91.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  skip_provider_registration = true
+}
+
 resource "azurerm_resource_group" "readings" {
-  name = "meter-values"
+  name     = "meter-values"
   location = "West Europe"
 }
 
+# Shared access keys are enabled for now,
+# as only blobs and queues can use only aad auth
+# tables and files can only use keys (for now)
 resource "azurerm_storage_account" "readings" {
-  name = "meter-values"
-  resource_group_name = azurerm_resource_group.readings.name
-  location = azurerm_resource_group.readings.location
-  account_tier = "Standard"
+  name                     = "metervalues"
+  resource_group_name      = azurerm_resource_group.readings.name
+  location                 = azurerm_resource_group.readings.location
+  account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_storage_table" "manualreadings" {
-    name = "manual-readings"
-    storage_account_name = azurerm_storage_account.readings.name
+  name                 = "manualreadings"
+  storage_account_name = azurerm_storage_account.readings.name
 }
