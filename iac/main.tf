517d69d1237802fe2d1ad3b65dbfa36986b3f053
louis <1160507+naiaden@users.noreply.github.com>
2022-01-19 22:17:35 +0100

    fix(terraform): conform to rules

--- iac/main.tf
+++ iac/main.tf
@@ -1,26 +1,26 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name = "tfstate"
-    storage_account_name = "naiadentfstate"
-    container_name = "tfstate"
-    key = "perimetron.tfstate"
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "tfstate"
+#     storage_account_name = "naiadentfstate"
+#     container_name       = "tfstate"
+#     key                  = "perimetron.tfstate"
+#   }
+# }
 
 resource "azurerm_resource_group" "readings" {
-  name = "meter-values"
+  name     = "meter-values"
   location = "West Europe"
 }
 
 resource "azurerm_storage_account" "readings" {
-  name = "meter-values"
-  resource_group_name = azurerm_resource_group.readings.name
-  location = azurerm_resource_group.readings.location
-  account_tier = "Standard"
+  name                     = "metervalues"
+  resource_group_name      = azurerm_resource_group.readings.name
+  location                 = azurerm_resource_group.readings.location
+  account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_storage_table" "manualreadings" {
-    name = "manual-readings"
-    storage_account_name = azurerm_storage_account.readings.name
+  name                 = "manualreadings"
+  storage_account_name = azurerm_storage_account.readings.name
 }
